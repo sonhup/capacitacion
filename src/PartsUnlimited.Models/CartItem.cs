b7d85aac86eb414e8f365f2b03d7ab044a6d529b
SoniaH <sonhup@gmail.com>
2024-05-15 14:47:13 -0500

    prueba

--- src/PartsUnlimited.Models/CartItem.cs
+++ src/PartsUnlimited.Models/CartItem.cs
@@ -6,7 +6,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PartsUnlimited.Models
 {
-    //prueba de cambios
+    //prueba de cambios release
     public class CartItem
     {
         [Key]
